8e98485e2d5fa44a6b1402f5c987e9e5b5ce34c7
Alexis <agarcia196@outlook.com>
2018-03-09 09:45:05 -0500

    Clase Persona
    Actualización

--- src/clasesBodega/Persona.java
+++ src/clasesBodega/Persona.java
@@ -1,3 +1,11 @@
+/**
+ * clasesBodega: Recursos.
+ *
+ * @author Alexis García Ramirez 
+ * @author Jeisson Harvey Barrera Sanchez
+ * @version 9.3.2018
+ * 
+ */
 package clasesBodega;
 
 public abstract class Persona {
@@ -7,19 +15,41 @@ public abstract class Persona {
 	protected String correo;
 	protected String cc;
 	protected String tipoID;
-	public Persona(String nombre, String apellido, String genero, String correo, String cc, String tipoID) {
+	protected String pwd;
+	
+	/**
+	 * Instantiates a new persona.
+	 *
+	 * @param nombre Nombre de la persona
+	 * @param apellido el apellido
+	 * @param genero el genero
+	 * @param correo el correo
+	 * @param cc Numero de cedula
+	 * @param tipoID el tipo cedula
+	 * @param pwd la contraseña
+	 */
+	public Persona(String nombre, String apellido, String genero, String correo, String cc, String tipoID, String pwd) {
+		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.genero = genero;
 		this.correo = correo;
 		this.cc = cc;
 		this.tipoID = tipoID;
+		this.pwd = pwd;
 	}
 	public String getCorreo() {
 		return correo;
 	}
+	
 	public void setCorreo(String correo) {
 		this.correo = correo;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
 	}
 	public String getNombre() {
 		return nombre;
@@ -35,9 +65,6 @@ public abstract class Persona {
 	}
 	public String getTipoID() {
 		return tipoID;
-	}
+	}	
 	
-	
-	
-
 }
