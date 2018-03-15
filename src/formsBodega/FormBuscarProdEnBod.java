6b7dbd531ed27930ad2ff05cb0423665c2aedfce
JLSC24 <32958021+JLSC24@users.noreply.github.com>
2018-03-15 01:11:36 -0500

    Actualizar form de productos en bodega

--- src/formsBodega/FormBuscarProdEnBod.java
+++ src/formsBodega/FormBuscarProdEnBod.java
@@ -60,7 +60,7 @@ public class FormBuscarProdEnBod extends JFrame implements Serializable{
 		this.persona=persona;
 		this.empresa=empresa;
 		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 878, 729);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
